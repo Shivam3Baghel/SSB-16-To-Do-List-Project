e7e534fc691e8dc328638588952a5d60fdfd8f5d
Shivam Baghel SBC1 <shivam3baghel@gmail.com>
2026-03-27 23:06:49 +0530

    27 3 2026 initial commit

---
+++ src/script.js
@@ -0,0 +1,24 @@
+let input=prompt("What would you like to do?");
+const todos=["Revise HTML","Revise CSS","Revise Bootstrap","Revise Javascript","Revise Node.js","Revise Express.js","Revise MongoDB","Revise React.js"];
+while(input!=="quit" && input !=='q'){
+    if(input==="list"){
+        console.log("**********");
+        for(let i=0;i<=todos.length-1;i++){
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log("**********");
+    }else if(input==="new"){
+        const newTodo=prompt('Ok, what is the new todo?');
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list!`);
+    }else if(input==="delete"){
+        const index=parseInt(prompt("Ok, enter an index to delete:"));
+        if(!Numnber.isNaN(index)){}
+        const deleted=todos.splice(index,1);
+        console.log(`Ok, deleted ${deleted[0]}`);
+    }else{
+        console.log("Unknown index.");
+    }
+    input=prompt("What would you like to do?");
+}
+console.log("OK, quit the app!.");
